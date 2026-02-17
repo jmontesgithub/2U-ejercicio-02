84c7c5f9b7adda2469f1fa908119bf6637a36a97
jmontesgithub <jmontes@ittepic.edu.mx>
2026-02-17 09:44:35 -0700

    Feat: Clase Vehiculo incompleta

---
+++ Proy_venta/Proy_U2_ejercicio_02/src/Vehiculo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int anio;
+    private double precio;
+    private double capacidadCarga;
+    
+    public Vehiculo(){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio=anio;
+        this.precio=precio;
+        this.capacidadCarga=capacidadCarga;
+    
+    }
+    
+}
